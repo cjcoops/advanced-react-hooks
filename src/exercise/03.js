b32e1c7fc16a8008ee5f72a5fd7660c7e4eb8e2e
Chris Cooper <chris.cooper@abcellera.com>
2023-10-23 16:48:08 -0700

    exercise-3

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -16,13 +16,20 @@ function CountProvider(props) {
   return <CountContext.Provider value={[count, setCount]} {...props} />
 }
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context)
+    throw new Error('useCount may on be used inside a CountProvider')
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
 
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
